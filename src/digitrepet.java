0365a56128daeb055bd03d362c80e83ce3465a8f
Nagesh-tri <78202038+Nagesh-tri@users.noreply.github.com>
2022-09-27 15:27:54 +0530

    Create digitrepet.java
    count digit-repetaion in given number.

---
+++ src/digitrepet.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class digitrepet {
+    public static void main(String[] args) {
+        System.out.println("Enter the Number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("Number check for Repetation: ");
+        int rt = sc.nextInt();
+        sc.close();
+        int cont =0;
+        while(num%10>0){
+            if(num%10==rt){
+               cont++;
+            }
+            num=num/10;
+        }
+        System.out.println("Number "+rt+" is comes: "+cont+" times");
+    }
+    
+}
